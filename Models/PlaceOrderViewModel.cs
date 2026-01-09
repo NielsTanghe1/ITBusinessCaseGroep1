d81aad1a7d66c09c66ee799c4ebca1d78d8e116c
Rob1ndw <robindewolf130@gmail.com>
2026-01-09 14:00:09 +0100

    Hier is een eerste push met een webstore waar er fotos staan en allemaal andere snufjes

---
+++ Models/PlaceOrderViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITBusinessCase.Models;
+
+public class PlaceOrderViewModel {
+	// Customer details
+	[Required(ErrorMessage = "Voornaam is verplicht.")]
+	public string FirstName { get; set; } = "";
+
+	[Required(ErrorMessage = "Achternaam is verplicht.")]
+	public string LastName { get; set; } = "";
+
+	[Required(ErrorMessage = "Email is verplicht.")]
+	[EmailAddress(ErrorMessage = "Geef een geldig emailadres op.")]
+	public string Email { get; set; } = "";
+
+	// Shipping address
+	[Required(ErrorMessage = "Straat is verplicht.")]
+	public string Street { get; set; } = "";
+
+	[Required(ErrorMessage = "Postbus is verplicht.")]
+	public string Postbus { get; set; } = "";
+
+	[Required(ErrorMessage = "Stad is verplicht.")]
+	public string City { get; set; } = "";
+
+	[Required(ErrorMessage = "Postcode is verplicht.")]
+	public string Postcode { get; set; } = "";
+
+	[Required(ErrorMessage = "Land is verplicht.")]
+	public string Country { get; set; } = "";
+
+	// Products (meerdere)
+	public List<OrderItemInput> Items { get; set; } = new();
+}
+
+public class OrderItemInput {
+	public string ProductId { get; set; } = "";
+
+	// 0 = niet gekozen, 1..10 = gekozen
+	[Range(0, 10, ErrorMessage = "Aantal moet tussen 0 en 10 liggen.")]
+	public int Quantity { get; set; } = 0;
+}
