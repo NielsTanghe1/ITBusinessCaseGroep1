5f382d987982c1694cc7981f388905784e1ff7fd
CodeSmashing <123628904+CodeSmashing@users.noreply.github.com>
2026-01-20 13:38:49 +0100

    feat(web): add Coffee CRUD scaffolding
    Add complete CRUD functionality for Coffee management with controller and views.
    
    Changes:
    
    Add CoffeesController.cs (/Web/Controllers/).
    
    Add coffee CRUD views (/Web/Views/Coffees/):
    - Create.cshtml
    - Delete.cshtml
    - Details.cshtml
    - Edit.cshtml
    - Index.cshtml
    
    Update _Layout.cshtml (/Web/Views/Shared/) to include navigation link to CoffeesController.Index.

---
+++ Web/Controllers/CoffeesController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Data;
+using Models.Entities;
+
+namespace Web.Controllers;
+
+public class CoffeesController : Controller {
+	private readonly LocalDbContext _context;
+
+	public CoffeesController(LocalDbContext context) {
+		_context = context;
+	}
+
+	// GET: Coffees
+	public async Task<IActionResult> Index() {
+		return View(await _context.Coffees.ToListAsync());
+	}
+
+	// GET: Coffees/Details/5
+	public async Task<IActionResult> Details(long? id) {
+		if (id == null) {
+			return NotFound();
+		}
+
+		var coffee = await _context.Coffees
+			 .FirstOrDefaultAsync(m => m.Id == id);
+		if (coffee == null) {
+			return NotFound();
+		}
+
+		return View(coffee);
+	}
+
+	// GET: Coffees/Create
+	public IActionResult Create() {
+		return View();
+	}
+
+	// POST: Coffees/Create
+	// To protect from overposting attacks, enable the specific properties you want to bind to.
+	// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Create([Bind("Id,Name,Type,Price,CreatedAt,DeletedAt")] Coffee coffee) {
+		if (ModelState.IsValid) {
+			_context.Add(coffee);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+		return View(coffee);
+	}
+
+	// GET: Coffees/Edit/5
+	public async Task<IActionResult> Edit(long? id) {
+		if (id == null) {
+			return NotFound();
+		}
+
+		var coffee = await _context.Coffees.FindAsync(id);
+		if (coffee == null) {
+			return NotFound();
+		}
+		return View(coffee);
+	}
+
+	// POST: Coffees/Edit/5
+	// To protect from overposting attacks, enable the specific properties you want to bind to.
+	// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Type,Price,CreatedAt,DeletedAt")] Coffee coffee) {
+		if (id != coffee.Id) {
+			return NotFound();
+		}
+
+		if (ModelState.IsValid) {
+			try {
+				_context.Update(coffee);
+				await _context.SaveChangesAsync();
+			} catch (DbUpdateConcurrencyException) {
+				if (!CoffeeExists(coffee.Id)) {
+					return NotFound();
+				} else {
+					throw;
+				}
+			}
+			return RedirectToAction(nameof(Index));
+		}
+		return View(coffee);
+	}
+
+	// GET: Coffees/Delete/5
+	public async Task<IActionResult> Delete(long? id) {
+		if (id == null) {
+			return NotFound();
+		}
+
+		var coffee = await _context.Coffees
+			 .FirstOrDefaultAsync(m => m.Id == id);
+		if (coffee == null) {
+			return NotFound();
+		}
+
+		return View(coffee);
+	}
+
+	// POST: Coffees/Delete/5
+	[HttpPost, ActionName("Delete")]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> DeleteConfirmed(long id) {
+		var coffee = await _context.Coffees.FindAsync(id);
+		if (coffee != null) {
+			_context.Coffees.Remove(coffee);
+		}
+
+		await _context.SaveChangesAsync();
+		return RedirectToAction(nameof(Index));
+	}
+
+	private bool CoffeeExists(long id) {
+		return _context.Coffees.Any(e => e.Id == id);
+	}
+}
