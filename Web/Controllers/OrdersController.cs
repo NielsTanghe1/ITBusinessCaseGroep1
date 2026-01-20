4730d501ec2f1cebc35cea780cbdd1ce91b46580
CodeSmashing <123628904+CodeSmashing@users.noreply.github.com>
2026-01-20 15:59:33 +0100

    feat(Web): add Orders CRUD scaffolding
    Add complete CRUD functionality for Order management with controller and views.
    
    Changes:
    
    Add OrdersController.cs (/Web/Controllers/) with old code commented out.
    
    Add order CRUD views (/Web/Views/PaymentDetails/):
    - Create.cshtml (with old ode commented out)
    - Delete.cshtml
    - Details.cshtml
    - Edit.cshtml
    - Index.cshtml
    
    Update _Layout.cshtml (/Web/Views/Shared/) to include navigation link to OrdersController.Index.

--- Web/Controllers/OrdersController.cs
+++ Web/Controllers/OrdersController.cs
@@ -1,10 +1,9 @@
 ﻿using MassTransit;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Models.Data;
 using Models.Entities;
-using Models.Entities.DTO;
-using System.Security.Claims;
-using Web.Models;
 
 namespace Web.Controllers;
 
@@ -17,101 +16,192 @@ public class OrdersController : Controller {
 		_publish = publish;
 	}
 
-	// GET: /Orders/Create
-	[HttpGet]
-	public IActionResult Create() {
-		//ViewData["CoffeeUserId"] = new SelectList(_context.Users, "Id", "FirstName");
-		//return View();
-
-		bool parseResult = long.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out long id);
-		if (parseResult) {
-			return View(new OrderViewModel {
-				CoffeeUser = _context.Users.Find(id),
-				Address = _context.Addresses.Find(id)
-			});
-		}
-		return View();
+	// GET: Orders
+	public async Task<IActionResult> Index() {
+		var localDbContext = _context.Orders.Include(o => o.CoffeeUser);
+		return View(await localDbContext.ToListAsync());
 	}
 
-	// POST: /Orders/Create
+	// GET: Orders/Details/5
+	public async Task<IActionResult> Details(long? id) {
+		if (id == null) {
+			return NotFound();
+		}
+
+		var order = await _context.Orders
+			 .Include(o => o.CoffeeUser)
+			 .FirstOrDefaultAsync(m => m.Id == id);
+		if (order == null) {
+			return NotFound();
+		}
+
+		return View(order);
+	}
+
+	// GET: Orders/Create
+	public IActionResult Create() {
+		ViewData["CoffeeUserId"] = new SelectList(_context.Users, "Id", "FirstName");
+		return View();
+
+		//bool parseResult = long.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out long id);
+		//if (parseResult) {
+		//	return View(new OrderViewModel {
+		//		CoffeeUser = _context.Users.Find(id),
+		//		Address = _context.Addresses.Find(id)
+		//	});
+		//}
+		//return View();
+	}
+
+	// POST: Orders/Create
 	// To protect from overposting attacks, enable the specific properties you want to bind to.
 	// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 	[HttpPost]
 	[ValidateAntiForgeryToken]
-	public async Task<IActionResult> Create(OrderViewModel order) {
-		//if (ModelState.IsValid) {
-		//	_context.Add(order);
-		//	await _context.SaveChangesAsync();
-		//	return RedirectToAction(nameof(Index));
+	public async Task<IActionResult> Create([Bind("Id,CoffeeUserId,Status,CreatedAt,DeletedAt")] Order order) {
+		if (ModelState.IsValid) {
+			_context.Add(order);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+		ViewData["CoffeeUserId"] = new SelectList(_context.Users, "Id", "FirstName", order.CoffeeUserId);
+		return View(order);
+
+		//bool parseResult = long.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out long userId);
+		//Order newOrder = new() {
+		//	CoffeeUserId = userId
+		//};
+		//order.Items ??= new List<OrderItem>();
+
+		//// Minstens 1 product gekozen (qty > 0)
+		//if (!order.Items.Any(i => i.Quantity > 0))
+		//	ModelState.AddModelError("", "Kies minstens één product en zet het aantal groter dan 0.");
+
+		//if (!ModelState.IsValid) {
+		//	// Zorg dat alle items er blijven instaan (voor de view)
+		//	if (order.Items.Count == 0) {
+		//		order.Items = _context.Coffees
+		//			.Select(coffee => new OrderItem {
+		//				OrderId = newOrder.Id,
+		//				CoffeeId = coffee.Id,
+		//				Quantity = 0,
+		//				UnitPrice = coffee.Price
+		//			})
+		//			.ToList();
+		//	}
+		//	return View(order);
 		//}
-		//ViewData["CoffeeUserId"] = new SelectList(_context.Users, "Id", "FirstName", order.CoffeeUserId);
-		//return View(order);
 
-		bool parseResult = long.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out long userId);
-		Order newOrder = new() {
-			CoffeeUserId = userId
-		};
-		order.Items ??= new List<OrderItem>();
+		//var catalogById = _context.Coffees.ToDictionary(x => x.Id, x => x);
 
-		// Minstens 1 product gekozen (qty > 0)
-		if (!order.Items.Any(i => i.Quantity > 0))
-			ModelState.AddModelError("", "Kies minstens één product en zet het aantal groter dan 0.");
+		//var lines = order.Items
+		//	 .Where(i => i.Quantity > 0)
+		//	 .Select(i => {
+		//		 if (!catalogById.TryGetValue(i.CoffeeId, out var product))
+		//			 throw new Exception($"Onbekend product: {i.CoffeeId}");
 
-		if (!ModelState.IsValid) {
-			// Zorg dat alle items er blijven instaan (voor de view)
-			if (order.Items.Count == 0) {
-				order.Items = _context.Coffees
-					.Select(coffee => new OrderItem {
-						OrderId = newOrder.Id,
-						CoffeeId = coffee.Id,
-						Quantity = 0,
-						UnitPrice = coffee.Price
-					})
-					.ToList();
-			}
-			return View(order);
+		//		 var qty = i.Quantity;
+		//		 var lineTotal = product.Price * qty;
+
+		//		 return new OrderLine(Guid.NewGuid().ToString(), product.Id, product.Name.ToString(), product.Price, qty, lineTotal);
+		//	 })
+		//	 .ToList();
+
+		//var total = lines.Sum(l => l.LineTotal);
+		//var address = _context.Addresses.First(addr => addr.CoffeeUserId == order.CoffeeUser.Id);
+
+		//var message = new OrderSubmitted(
+		//	 Guid.NewGuid().ToString(),
+		//	 order.CoffeeUser.FirstName,
+		//	 order.CoffeeUser.LastName,
+		//	 order.CoffeeUser.Email,
+
+		//	 address.Type.ToString(),
+		//	 address.Street,
+		//	 address.HouseNumber,
+		//	 address.City,
+		//	 address.PostalCode,
+		//	 address.CountryISO,
+		//	 address.UnitNumber,
+
+		//	 total,
+		//	 lines
+		//);
+
+		//await _publish.Publish(message);
+
+		//TempData["OrderPlaced"] = $"Order sent to RabbitMQ! Totaal: €{total:0.00}";
+		//return RedirectToAction(nameof(Create));
+	}
+
+	// GET: Orders/Edit/5
+	public async Task<IActionResult> Edit(long? id) {
+		if (id == null) {
+			return NotFound();
 		}
 
-		var catalogById = _context.Coffees.ToDictionary(x => x.Id, x => x);
+		var order = await _context.Orders.FindAsync(id);
+		if (order == null) {
+			return NotFound();
+		}
+		ViewData["CoffeeUserId"] = new SelectList(_context.Users, "Id", "FirstName", order.CoffeeUserId);
+		return View(order);
+	}
 
-		var lines = order.Items
-			 .Where(i => i.Quantity > 0)
-			 .Select(i => {
-				 if (!catalogById.TryGetValue(i.CoffeeId, out var product))
-					 throw new Exception($"Onbekend product: {i.CoffeeId}");
+	// POST: Orders/Edit/5
+	// To protect from overposting attacks, enable the specific properties you want to bind to.
+	// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Edit(long id, [Bind("Id,CoffeeUserId,Status,CreatedAt,DeletedAt")] Order order) {
+		if (id != order.Id) {
+			return NotFound();
+		}
 
-				 var qty = i.Quantity;
-				 var lineTotal = product.Price * qty;
+		if (ModelState.IsValid) {
+			try {
+				_context.Update(order);
+				await _context.SaveChangesAsync();
+			} catch (DbUpdateConcurrencyException) {
+				if (!OrderExists(order.Id)) {
+					return NotFound();
+				} else {
+					throw;
+				}
+			}
+			return RedirectToAction(nameof(Index));
+		}
+		ViewData["CoffeeUserId"] = new SelectList(_context.Users, "Id", "FirstName", order.CoffeeUserId);
+		return View(order);
+	}
 
-				 return new OrderLine(Guid.NewGuid().ToString(), product.Id, product.Name.ToString(), product.Price, qty, lineTotal);
-			 })
-			 .ToList();
+	// GET: Orders/Delete/5
+	public async Task<IActionResult> Delete(long? id) {
+		if (id == null) {
+			return NotFound();
+		}
 
-		var total = lines.Sum(l => l.LineTotal);
-		var address = _context.Addresses.First(addr => addr.CoffeeUserId == order.CoffeeUser.Id);
+		var order = await _context.Orders
+			 .Include(o => o.CoffeeUser)
+			 .FirstOrDefaultAsync(m => m.Id == id);
+		if (order == null) {
+			return NotFound();
+		}
 
-		var message = new OrderSubmitted(
-			 Guid.NewGuid().ToString(),
-			 order.CoffeeUser.FirstName,
-			 order.CoffeeUser.LastName,
-			 order.CoffeeUser.Email,
+		return View(order);
+	}
 
-			 address.Type.ToString(),
-			 address.Street,
-			 address.HouseNumber,
-			 address.City,
-			 address.PostalCode,
-			 address.CountryISO,
-			 address.UnitNumber,
+	// POST: Orders/Delete/5
+	[HttpPost, ActionName("Delete")]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> DeleteConfirmed(long id) {
+		var order = await _context.Orders.FindAsync(id);
+		if (order != null) {
+			_context.Orders.Remove(order);
+		}
 
-			 total,
-			 lines
-		);
-
-		await _publish.Publish(message);
-
-		TempData["OrderPlaced"] = $"Order sent to RabbitMQ! Totaal: €{total:0.00}";
-		return RedirectToAction(nameof(Create));
+		await _context.SaveChangesAsync();
+		return RedirectToAction(nameof(Index));
 	}
 
 	private bool OrderExists(long id) {
